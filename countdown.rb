cb0deb0c1eca50342500802364f37ac0980494c5
Megan Collier <meganraec@live.com>
2018-02-26 21:07:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 def countdown(n)
   while n > 0
-    countdown_output = "'#{n}' SECOND(S)!\n"
+    countdown_output = "#{n} SECOND(S)!\n"
     n -= 1
     $stdout.write countdown_output
     break if n == 0
@@ -10,7 +10,7 @@ end
 
 def countdown_with_sleep(n)
   while n > 0
-    countdown_output = "'#{n}' SECOND(S)!\n"
+    countdown_output = "#{n} SECOND(S)!\n"
     sleep(1)
     n -= 1
     $stdout.write countdown_output
