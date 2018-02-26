b06f5528beae8444950f911d0d72990c8cce3c6b
Megan Collier <meganraec@live.com>
2018-02-26 21:08:22 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
+def countdown(n)
+  while n > 0
+    countdown_output = "#{n} SECOND(S)!\n"
+    n -= 1
+    $stdout.write countdown_output
+    break if n == 0
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(n)
+  while n > 0
+    countdown_output = "#{n} SECOND(S)!\n"
+    sleep(1)
+    n -= 1
+    $stdout.write countdown_output
+    break if n == 0
+  end
+  return "HAPPY NEW YEAR!"
 end
