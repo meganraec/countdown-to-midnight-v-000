347e3291cf37f652e82d833ca53594eb5e61325a
Megan Collier <meganraec@live.com>
2018-02-26 20:54:25 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,9 +10,10 @@ end
 
 def countdown_with_sleep(number)
   while number > 0
-    $stdout.write '#{number} SECOND(S)!\n'
+    countdown_output = $stdout.write '#{number} SECOND(S)!\n'
     sleep(1)
     number -= 1
+    countdown_output
     break if number == 0
   end
   return "HAPPY NEW YEAR!"
